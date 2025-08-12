b55dbb5bb588be0adbb8263b37cf76ea1950c205
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2025-08-12 09:48:42 +0530

    2nd commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,16 @@
 import "@/global.css";
+import { Link } from "expo-router";
 import { useState } from "react";
 import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
+
 export default function LoginScreen() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = () => {
-    // Placeholder login function
-    console.log("Email:", email);
-    console.log("Password:", password);
-    // Navigate to home after successful login
-  };
-
+  
   return (
     <View  className="flex-1 justify-center items-center bg-white">
       <Text className="text-1 text-dark-200 text-5xl font-bold">welcome</Text>
+
+     
     </View>
   );
 }
