095fc80d82c6149f810143c2b18b96650c5a1b9b
kidiezyllex <buitranthienan1111@gmail.com>
2025-09-28 14:57:12 +0700

    the most stable version currently

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,12 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+
+// Ensure proper module resolution for react-native-css-interop
+config.resolver.alias = {
+  ...config.resolver.alias,
+  'react-native-css-interop/jsx-runtime': 'react-native-css-interop/jsx-runtime',
+};
+
 module.exports = withNativeWind(config, { input: "./global.css" });
